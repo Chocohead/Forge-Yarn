504848a1c1caa248c6d059d4304dd3040ad392e7
Chocohead <Chocohead@Aroma1997.org>
2019-12-18 13:16:34 +0000

    Allow increasing the logging verbosity to see what Gradle is up to
    Can be disabled by passing -quietGradle=true

--- jeka/def/Build.java
+++ jeka/def/Build.java
@@ -7,6 +7,9 @@ import java.nio.file.StandardCopyOption;
 
 import dev.jeka.core.api.file.JkPathTree;
 import dev.jeka.core.api.java.JkJavaProcess;
+import dev.jeka.core.api.java.project.JkJavaProject;
+import dev.jeka.core.api.system.JkLog;
+import dev.jeka.core.api.system.JkLog.Verbosity;
 import dev.jeka.core.api.utils.JkUtilsPath;
 import dev.jeka.core.tool.JkCommands;
 import dev.jeka.core.tool.JkConstants;
@@ -15,6 +18,7 @@ import dev.jeka.core.tool.JkImport;
 @JkImport("libs/*jar")
 class Build extends JkCommands {
 	private static final String SETUP_DIR = JkConstants.JEKA_DIR + "/setup";
+	public boolean quietGradle; //Run with -quietGradle=true
 
 	@Override
 	protected void setup() {
@@ -24,7 +28,7 @@ class Build extends JkCommands {
 		//doGradlePart(setupDir, "Forge", "setup", "eclipse", "--no-daemon");
 	}
 
-	private static void doGradlePart(Path setupDir, String name, String... args) {
+	private void doGradlePart(Path setupDir, String name, String... args) {
 		Path classpath = setupDir.resolve(name + ".classpath");
 		Path settings = setupDir.resolve(name);
 		Path hashes = setupDir.resolve(name + "-hashes.txt");
@@ -36,13 +40,28 @@ class Build extends JkCommands {
 			//Move the stuff over needed for building
 			JkPathTree.of(settings).copyTo(build, StandardCopyOption.COPY_ATTRIBUTES);
 
+			boolean increasedLogging = false;
+			if (!quietGradle && JkLog.verbosity() == Verbosity.MUTE) {
+				//When running without -LH (log headers) nothing will get logged from Gradle's output
+				increasedLogging = true;
+				JkLog.setVerbosity(Verbosity.NORMAL);
+			}
+
 			//Invoke Gradle, hope it does what it's meant to
 			JkJavaProcess.of().withWorkingDir(build).withClasspath(build.resolve("gradle/wrapper/gradle-wrapper.jar"))
 			.andOptions("-Dorg.gradle.appname=Build").runClassSync("org.gradle.wrapper.GradleWrapperMain", args);
 
+			JkLog.startTask("Starting extractor");
 			Path merge = setupDir.resolve("Merge");
 			ClassPathExtractor.main(new String[] {build.toAbsolutePath().toString(),
 					merge.resolve("remap.jar").toAbsolutePath().toString(), merge.toAbsolutePath().toString()});
+			JkLog.endTask();
+
+			//Remember to set the logging back if it was changed
+			if (increasedLogging) JkLog.setVerbosity(Verbosity.MUTE);
+
+			//Clean up the stuff used for building
+			JkPathTree.of(build).andMatching(false, "gradle/**").deleteContent();
 		}
 	}
 
